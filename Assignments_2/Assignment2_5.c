8f748286fa0bf3e42c30e11beecde3644add069f
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-08-10 22:05:19 +0530

    Add files via upload

---
+++ Assignments_2/Assignment2_5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool ChkEven(int iNo)
+{
+	int iCnt=0;
+
+	if(iNo % 2 == 0)
+	{
+	  return 1;
+	}
+	else
+	{
+       return 0;
+	}
+}
+
+int main()
+{
+	int iValue=0;
+	bool bRet=0;
+
+	printf("Enter the value:\n");
+	scanf("%d",&iValue);
+
+	bRet=ChkEven(iValue);
+
+	if(bRet== 1)
+	{
+	  printf("\n%d Number is Even",iValue);
+	}
+	else
+	{
+	  printf("\n%d Number is Odd",iValue);
+	}
+
+
+
+	return 0;
+}
