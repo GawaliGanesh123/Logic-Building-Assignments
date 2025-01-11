410ed12e8c76cb2deb7e86ef2c26eb1ed8cdecf9
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-11 14:55:37 +0530

    Question_4.c

---
+++ Assignment_2/Question_4.c
@@ -0,0 +1,35 @@
+/* Accept two numbers from user and display first number in second number of times.
+
+Input- 12 5
+Output: 12 12 12 12 12
+
+*/ 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+void Display(int iNo1, int iNo2)
+{
+	int iCnt=0;
+
+	for(iCnt=1; iCnt<=iNo2; iCnt++)
+	{
+		printf("%d\t",iNo1);
+	}
+}
+int main()
+{
+	int iValue1=0;
+	int iValue2=0;
+
+	printf("Enter the first Number:");
+	scanf("%d",&iValue1);
+
+	printf("Enter the frequency Number:");
+	scanf("%d",&iValue2);
+
+	Display(iValue1,iValue2);
+
+	return 0;
+}
