0f1dad0beeccbf9b30b17c7162c48517b41f8271
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-11 14:45:49 +0530

    Question_1.c

---
+++ Assignment_2/Question_1.c
@@ -0,0 +1,28 @@
+/* Accept one number from user and print that number of * on Screen Using For loop.
+
+Input- 5
+Output- * * * * *
+
+*/
+
+#include<stdio.h>
+
+void Accept(int iNo)
+{
+	int iCnt=0;
+
+	for(iCnt=1; iCnt<=iNo; iCnt++)
+	{
+	  printf("*\t");
+	}
+}
+int main()
+{
+	int iValue=0;
+
+	printf("Enter the number:\n");
+	scanf("%d",&iValue);
+
+	Accept(iValue);
+	return 0;
+}
