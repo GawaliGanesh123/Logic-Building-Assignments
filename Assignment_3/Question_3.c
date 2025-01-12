d80236c0379bc701aa97edf26b99da7841cab9c4
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-12 23:56:55 +0530

    Question_3.c

---
+++ Assignment_3/Question_3.c
@@ -0,0 +1,47 @@
+/* Write a program which accept number from user and print even factors of that number.
+
+Input: 36
+Output: 3 9 
+
+*/
+
+
+#include<stdio.h>
+
+
+void DisplayOddFactor(int iNo)
+{
+	int iCnt=0;
+
+	if(iNo<=0)
+	{
+		iNo = -iNo;
+	}
+     
+     for(iCnt=1; iCnt<=iNo; iCnt++)
+     {
+     	if(iNo % iCnt ==0)
+     	{
+     		if(iCnt % 2 != 0)
+     		{
+     			printf("\n%d\t",iCnt);
+     		}
+     		
+     	}
+     }
+}
+
+int main()
+{
+	int iValue=0;
+	
+
+	printf("Enter the value:\n");
+	scanf("%d",&iValue);
+     
+     DisplayOddFactor(iValue);
+
+
+	return 0;
+}
+  
