bc7501d13b68a1bae003f65c7b87c72df8fcf31f
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-16 18:13:59 +0530

    Question_3.c

--- Assignment_5/Question_3.c
+++ Assignment_5/Question_3.c
@@ -1,42 +1,31 @@
+/* Write a program which accept number from user and print its number line.
 
-/* Write a program which accept number from user and display all its non factors.
-
-Input :12 
-Output: 5 7 8 9 10 11
-
-Input: 13
-Output: 2 3 4 5 6 7 8 9 10 11 12
-
-Input:10
-Output: 3 4 6 7 8 9
+Input- 4
+Output- -4	-3	-2	-1	0	1	2	3	4 
 
 */
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
-void NonFact(int iNo)
+void NumberLine(int iNo)
 {
-	int iCnt=0;
+	 int iCnt=0;
 
-	for(iCnt=1; iCnt<iNo; iCnt++)
-	{
-		if(iNo % iCnt != 0)
-		{
-			printf("%d\t",iCnt);
-		}
-	}
+	 for(iCnt=-iNo; iCnt<=iNo; iCnt++)
+	 {
+	    printf("%d\t",iCnt);
+	 }
 }
-
 
 int main()
 {
-  int iValue=0;
+    int iValue=0;
 
-  printf("Enter the Number:\n");
-  scanf("%d",&iValue);
+    printf("Enter the Number:\n");
+    scanf("%d",&iValue);
 
-  NonFact(iValue);
+    NumberLine(iValue);
 
 	return 0;
 }
