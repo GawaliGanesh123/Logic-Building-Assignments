265315f97494c8b1ac9b25b1491578ccde4eca2d
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-16 18:16:02 +0530

    Question_5.c

--- Assignment_5/Question_5.c
+++ Assignment_5/Question_5.c
@@ -1,49 +1,32 @@
+/* Write a program which accept Number from user and print first 5 multiple of Number.
 
-/* Write a program which accept number from user and return difference between summation of all its factors and non factors.
-
-Input: 12
-Output: -34  (16-50)
-
-Input : 10
-Output:-29   (8-37)
+Input- 4
+Output- 4	8	12	16	20
 
 */
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 #include<stdio.h>
 
-int FactDiff(int iNo)
+void MultipleOfNumber(int iNo)
 {
-	int iCnt=0;
-	int iSum=0;
-  int iDiff=0;
+    int iCnt=0;
 
-	for(iCnt=1; iCnt<iNo; iCnt++)
-	{
-	  if(iNo % iCnt == 0)
-	  {
-	    iSum= iSum+iCnt;
-	  }
-	  else
-	  {
-	    iDiff= iDiff+iCnt;
-	  }
-	}
-	  return iSum-iDiff;
+    for(iCnt=1; iCnt<=5; iCnt++)
+    {
+        printf("%d\t",iNo*iCnt); 
+    }
 }
 
 int main()
 {
     int iValue=0;
-    int iRet=0;
 
     printf("Enter the Number:\n");
     scanf("%d",&iValue);
 
-    iRet=FactDiff(iValue);
-
-    printf("Difference is:%d\n",iRet);
+    MultipleOfNumber(iValue);
 
 	return 0;
 }
