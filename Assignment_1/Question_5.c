8f3673fbebfb1d7055bad3b5ce23784816580db8
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-16 18:36:33 +0530

    Question_5.c

--- Assignment_1/Question_5.c
+++ Assignment_1/Question_5.c
@@ -1,23 +1,34 @@
-// Accept one number from user and print that number of * on Screen.
+/* Write a program which accept Number from user and Display its Table.
+
+Input- 2
+Output- 2	4	6	8	10	12	14	16	18	20
+
+Output- 5
+Output- 5	10	15	20	25	30	35	40	45	50
+
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
-void Accept(int iNo)
+void TableOfNumber(int iNo)
 {
-	int iCnt=0;
+   int iCnt=0;
 
-	for(iCnt=1; iCnt<=iNo; iCnt++)
-	{
-	  printf("*\t");
-	}
+   for(iCnt=1; iCnt<=10; iCnt++)
+   {
+      printf("%d\t",iNo*iCnt);
+   }
 }
+
 int main()
 {
-	int iValue=0;
+   int iValue=0;
 
-	printf("Enter the number:\n");
-	scanf("%d",&iValue);
+   printf("Enter The Number:\n");
+   scanf("%d",&iValue);
 
-	Accept(iValue);
-	return 0;
+   TableOfNumber(iValue);
+
+   return 0;
 }
