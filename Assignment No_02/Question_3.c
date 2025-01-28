ee8375244c2817258481207a269d0995d9c72919
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-28 14:11:17 +0530

    Create Question_3.c

---
+++ Assignment No_02/Question_3.c
@@ -0,0 +1,35 @@
+/ * Accept one number from user if number is less than 10 then print "Hello" otherwise print "Demo".
+
+Input- 6
+Output- Hello
+
+Input- 13
+Output- Demo
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	if(iNo<10)
+	{
+		printf("Hello");
+	}
+	else
+	{
+		printf("Demo");
+	}
+}
+int main()
+{
+   int iValue=0;
+
+   printf("Enter the number:");
+   scanf("%d",&iValue);
+
+   Display(iValue);
+
+	return 0;
+}
