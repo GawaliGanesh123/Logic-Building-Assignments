161413a0c3b02ef31acf67760a4cd60e30218fd7
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-28 14:10:29 +0530

    Create Question_2.c

---
+++ Assignment No_02/Question_2.c
@@ -0,0 +1,32 @@
+/* Accept one number from user and print that number of * on Screen using while loop.
+
+Input - 7
+Output- * * * * * * *
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void StarPrint(int iNo)
+{
+	int iCnt=0;
+
+	while(iCnt<iNo)
+	{
+		printf("*");
+
+        iCnt++;
+	}
+
+}
+int main()
+{
+	int iValue=0;
+
+	printf("Enter the number:\n");
+	scanf("%d",&iValue);
+
+	StarPrint(iValue);
+	return 0;
+}
