9f60fab4cc88a073f88239855c79e3dd46da2e37
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-01-29 14:25:30 +0530

    Create Question_5.c

---
+++ Assignment No_03/Question_5.c
@@ -0,0 +1,48 @@
+/* Accept one character from user and check whether that character is vowel (a,e,i,o,u) or not.
+  
+Input: e
+Output: TRUE
+  
+Input: d
+Output: FALSE
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int CheckVowel(char CValue)
+{
+
+  if(CValue=='a'|| CValue=='e' || CValue=='i' || CValue=='o' || CValue=='u')
+  {
+  	return 1;
+  }
+  else
+  {
+  	return 0;
+  }
+
+}
+
+int main()
+{
+	 char iChar='\0';
+	 int iRet=0;
+	printf("Enter the Character:\n");
+	scanf("%c",&iChar);
+     
+    iRet=CheckVowel(iChar);
+      if(iRet == 1)
+      {
+      	printf("%c\nCharacter is vowel",iChar);
+      }
+      else
+      {
+      	printf("%C\nCharacter is not vowel",iChar);
+      }
+
+	return 0;
+}
