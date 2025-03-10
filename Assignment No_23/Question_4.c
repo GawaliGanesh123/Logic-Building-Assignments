fafb99491fffad03f76893686a155c30132b9fc0
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-03-10 10:23:31 +0530

    Create Question_4.c

---
+++ Assignment No_23/Question_4.c
@@ -0,0 +1,50 @@
+/*4. Accept number of rows and number of columns from user and display below pattern.
+
+Input : iRow = 6 iCol = 5
+
+Output : * * * * *
+			   * @ @ @ *
+			   * @ @ @ *
+			   * @ @ @ *
+			   * @ @ @ *
+			   * * * * *
+			
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+
+void DisplayPattern(int iRow, int iCol)
+{
+	int i=0,j=0;
+
+	for(i=1; i<=iRow; i++)
+	{
+	   for(j=1; j<=iCol; j++)
+	   {
+	      if((i==1) || (i==iRow) || (j==1) || (j==iCol))
+	      printf("*\t");
+	      else
+	      printf("@\t");
+	   }
+	   printf("\n");
+	}
+}
+
+
+int main()
+{
+   int iValue1=0, iValue2=0;
+
+   printf("Enter a Number of Rows:");
+   scanf("%d",&iValue1);
+
+   printf("Enter a Number of Columns:");
+   scanf("%d",&iValue2);
+
+   DisplayPattern(iValue1, iValue2);
+
+	return 0;
+}
