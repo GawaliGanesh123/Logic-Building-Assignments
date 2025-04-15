1ac23ef22c67dabae49286a77890d5898c7575e0
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-04-15 11:18:24 +0530

    Create Question_1.c

---
+++ Assignment No_26/Question_1.c
@@ -0,0 +1,24 @@
+//1. Write a program which display ASCII table. Table contains symbol, decimal, hexadecimal and octal representation of every member from 0 to 255.
+
+
+
+#include <stdio.h>
+#define END 255
+
+void Display_ascii_table()
+{
+    int start = 0;
+    while (start <= END)
+    {
+        printf("Symbol\tDecimal\tHexadec\tOctal\n");
+        printf("%c\t%d\t%x\t%o\n", start, start, start, start);
+        start++;
+    }
+}
+
+int main()
+{
+    Display_ascii_table();
+
+    return 0;
+}
