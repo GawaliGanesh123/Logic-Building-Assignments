2b349b2a98f4f242aebd024bed26736f7c0f82c2
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-04-21 10:12:19 +0530

    Create Question_5.c

---
+++ Assignment No_31/Question_5.c
@@ -0,0 +1,40 @@
+/*5.Write a program which accept string from user and copy that characters of string into another string by toggling the case.
+
+Input : "Marvellous Python 2"
+Output : "mARVELLOUS pYTHON 2"
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+char strcpytoggle(char *src, char *dest)
+{
+    while (*src != '\0')
+    {
+        if (*src >= 'A' && *src <= 'Z')
+            *dest = *src + 32;
+        else if (*src >= 'a' && *src <= 'z')
+            *dest = *src - 32;
+        else
+            *dest = *src;
+        src++;
+        dest++;
+    }
+    *dest = '\0';
+
+}
+
+int main()
+{
+    char str1[30];
+    char str2[30];
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s", str1);
+
+    strcpytoggle(str1, str2);
+
+    printf("Toggled copied string is : %s\n", str2);
+
+    return 0;
+}
