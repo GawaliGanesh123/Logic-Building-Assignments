3da24f5ee8087f538a830926fe20f46c683dd91a
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-04-22 12:42:48 +0530

    Create Question_1.c

---
+++ Assignment No_32/Question_1.c
@@ -0,0 +1,37 @@
+/*
+1.Write a program which checks whether 15th bit is ON or OFF.
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int CheckBit(int iNo)
+{
+    int result = 0;
+    int iMask = 0x00004000;
+    
+    result = iMask & iNo;
+
+    if (result == iMask)
+        return 1;
+    return 0;
+}
+
+int main()
+{
+    int iNum=0;
+    int iResult=0;
+
+    printf("Enter number : ");
+    scanf("%d", &iNum);
+
+    iResult = CheckBit(iNum);
+
+    if (iResult == 1)
+        printf("Bit is on\n");
+    else
+        printf("Bit is off\n");
+
+    return 0;
+}
