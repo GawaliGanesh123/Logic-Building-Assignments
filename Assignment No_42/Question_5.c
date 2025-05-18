71ad12541e846f146a862321fa60d49014f94f5b
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-05-18 23:57:46 +0530

    Create Question_5.c

---
+++ Assignment No_42/Question_5.c
@@ -0,0 +1,41 @@
+/* 5. Write a recursive program which accept number from user and return its product of digits.
+
+Input : 523
+Output : 30
+
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int multiplication_of_digits(int num)
+{
+    static int mult = 1;
+
+    if (num)
+    {
+        mult = mult * (num % 10);
+        multiplication_of_digits(num / 10);
+    }
+    return mult;
+}
+
+int main()
+{
+    int num;
+    int result;
+
+    printf("Enter number : ");
+    scanf("%d", &num);
+
+    result = multiplication_of_digits(num);
+
+    printf("Multiplication of digits is : %d\n", result);
+
+    return 0;
+}
+
+
+
+
+
